f0efbf12da3c08fe7703dda615466198baf7da93
Selimmmm <selim.mellouk.halgand@proton.me>
2025-12-14 01:27:42 +0100

    docs and redocs and stuff

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,40 @@ from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from backend.settings import static_files, ENV
 from backend.domain_config import PRODUCTION_HOSTS
 from backend.lifespan import lifespan
+
 from backend.routers.root import router as root_router
 from backend.routers.trame import router as trame_router
 
 
+if ENV == "PRODUCTION":
+    docs_url = None
+    redoc_url = None
+    openapi_url = None
+else:
+    docs_url = "/docs"
+    redoc_url = "/redoc"
+    openapi_url = "/openapi.json"
+
+# app = FastAPI(
+#     title="pca-app",
+#     description="App Backend Server + Landing",
+#     version=settings.VERSION,  # TODO: make it dynamic with git tag ?
+#     lifespan=lifespan,
+#     docs_url=docs_url,
+#     redoc_url=redoc_url,
+#     openapi_url=openapi_url,
+# )
+
+
 # Create FastAPI app instance with lifespan
-app = FastAPI(title="Attam App", version="0.0.1", lifespan=lifespan)
+app = FastAPI(
+    title="Attam App",
+    version="0.0.1",
+    lifespan=lifespan,
+    docs_url=docs_url,
+    redoc_url=redoc_url,
+    openapi_url=openapi_url,
+)
 
 # Security: Only restrict hosts in production
 if ENV == "PRODUCTION":
